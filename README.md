b41d63a72735197a521c0774139d10f914b9372d
Onur Yemez <123839486+Onuryemez54@users.noreply.github.com>
2023-10-22 01:59:40 +0300

    TodoList Project

---
+++ README.md
@@ -0,0 +1,9 @@
+This repository contains the to-do list project.
+
+You can add the to-do that you entered in the Enter the to-do section to the list by using the Add to-do button.
+
+You can delete the list item you added by clicking the X sign.
+
+You can find the to-do job you want to find with the Search to-do section.
+
+You can delete all content with the Clean button.
---
+++ app.js
@@ -0,0 +1,183 @@
+
+
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners(); 
+
+function eventListeners() {
+
+    form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded", loadAllTodosToUI);
+    secondCardBody.addEventListener("click", deleteTodo);
+    filter.addEventListener("keyup", filterTodos);
+    clearButton.addEventListener("click", clearAllTodos);
+}
+
+
+
+function addTodo(e) {
+
+    const newTodo = todoInput.value.trim();
+
+    if (newTodo === "") {
+        showAlert("danger", "Please enter a todo...");
+    }
+    else {
+        addTodoToUI(newTodo);   
+        addTodoToStorage(newTodo);
+        showAlert("success", "Todo was successfully entered...");
+    }
+
+    e.preventDefault()
+}
+
+
+function showAlert(type, message) {
+
+    const alert = document.createElement("div");
+
+    alert.className = `alert alert-${type}`;
+
+    alert.textContent = message;
+
+    firstCardBody.appendChild(alert);
+
+    setTimeout(     
+        function () {
+            alert.remove();
+        }, 1000
+    );
+
+}
+
+
+
+function addTodoToUI(newTodo) {
+
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between";
+
+    const link = document.createElement("a");
+    link.href = "#";
+    link.className = "delete-item";
+    link.innerHTML = "<i class='fa fa-remove'></i>";
+
+    listItem.appendChild(document.createTextNode(newTodo));
+
+    listItem.appendChild(link);
+
+    todoList.appendChild(listItem);
+
+    todoInput.value = "";
+
+}
+
+function getTodosFromStorage() {
+    let todos;
+
+    if (localStorage.getItem("todos") === null) {
+
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+}
+
+
+
+
+function addTodoToStorage(newTodo) {
+
+
+    let todos = getTodosFromStorage();
+
+    todos.push(newTodo);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+
+
+
+function loadAllTodosToUI() {
+
+    let todos = getTodosFromStorage();
+
+    todos.forEach(
+        function (todo) {
+            addTodoToUI(todo);  
+        }
+    )
+}
+
+
+
+function deleteTodo(e) {
+
+    if (e.target.className === "fa fa-remove") {
+
+        e.target.parentElement.parentElement.remove();
+
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent); 
+
+        showAlert("success", "Todo was successfully deleted...");
+    }
+}
+
+
+
+
+function deleteTodoFromStorage(deleteFromStorage) {
+
+    let todos = getTodosFromStorage();
+
+    todos.forEach(
+        function (todo, index) {
+            if (todo === deleteFromStorage) {
+                todos.splice(index, 1);
+            }
+        }
+    )
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+
+function filterTodos(e) {
+    const filterValue = e.target.value.toLowerCase();
+
+    const listItems = document.querySelectorAll(".list-group-item");
+
+    listItems.forEach(
+        function (lists) {
+            const text = lists.textContent.toLowerCase();
+
+            if (text.indexOf(filterValue) === -1) {
+                lists.setAttribute("style", "display : none !important");
+            } else {
+                lists.setAttribute("style", "display : block");
+            }
+        }
+    )
+}
+
+
+
+
+function clearAllTodos(e) {
+    if (confirm("Are you sure you want to delete all?")) {
+        while (todoList.firstElementChild !== null) {
+            todoList.removeChild(todoList.firstElementChild); 
+        }
+    }
+
+  
+    localStorage.removeItem("todos");
+}
